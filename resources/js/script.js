eb2c52ad693f8367258f926c683ef3116678d69f
Michael Pierre <kaizen1971@gmail.com>
2015-03-17 23:27:50 -0400

    Bugfix: javascript input flow

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,4 +5,14 @@ $(document).ready(function(){
 		if($(this).val().length==$(this).attr('maxlength'))
 			$(this).next(':input').focus()
 	})
+
+	$('#input4').keyup(function(e){
+			if($(this).val().length==$(this).attr('maxlength'))
+				$('#input5').focus()
+	})
+
+	$('#input5,#input6,#input7').keyup(function(e){
+		if($(this).val().length==$(this).attr('maxlength'))
+			$(this).next(':input').focus()
+	})
 })
