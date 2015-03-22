5e6c120065d1106496a81cdb5005b0caddee37de
Michael Pierre <kaizen1971@gmail.com>
2015-03-22 12:59:02 -0400

    Refactor: CSS standards

--- resources/js/script.js
+++ resources/js/script.js
@@ -4,20 +4,32 @@ $(document).ready(function(){
 	$('#input1,#input2,#input3').keyup(function(e){
 		if($(this).val().length==$(this).attr('maxlength'))
 			$(this).next(':input').focus()
-	})
+	});
 
 	$('#input4').keyup(function(e){
 			if($(this).val().length==$(this).attr('maxlength'))
 				$('#input5').focus()
-	})
+	});
 
 	$('#input5,#input6,#input7').keyup(function(e){
 		if($(this).val().length==$(this).attr('maxlength'))
 			$(this).next(':input').focus()
-	})
+	});
 
 	$('#input1,#input2,#input3,#input4,#input5,#input6,#input7,#input8').focus(function(){
 			$(this).val('');
 			$(this).attr('placeholder','');
+	});
+
+	/*
+	$('#input8').focus(function(){
+		$('ip').colorFlow({
+		background: ['#85144b','#F012BE','#FFBC00', '#7FDBFF', '#01FF70'],
+		text: ['#CF5D94', '#EFA9FA', '#665800', '#004966', '#00662C'],
+		time: 25
+	});	
 	})
+	*/
 })
+
+
